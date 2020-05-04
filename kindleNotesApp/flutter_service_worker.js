a3d95a45259758be7651cc6d26b8c9e0fcaa85d0
ephammer <kidon.613@gmail.com>
2020-05-04 18:09:59 +0300

    Kindle Highlights App improvement

--- kindleNotesApp/flutter_service_worker.js
+++ kindleNotesApp/flutter_service_worker.js
@@ -3,12 +3,16 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "5910e380764b5b7e9a4443fe70a8e60c",
 "/": "5910e380764b5b7e9a4443fe70a8e60c",
-"main.dart.js": "810d3f01bcc48db4590765ae78b4ba88",
+"main.dart.js": "460d78592b9e77b75309127fed524132",
 "assets/LICENSE": "68ab651235daf7277fb3661d02b28fe8",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/AssetManifest.json": "22f84947390aac3fa0798c2cfe764f1d",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/icons/bookshelve.svg": "0b671f19b6ed8d5cad559d7169e05563",
+"assets/icons/bookshelve.png": "40d731baea8cd72811a53672bb116e9b",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/assets/icons/bookshelve.svg": "0b671f19b6ed8d5cad559d7169e05563",
+"assets/assets/icons/bookshelve.png": "40d731baea8cd72811a53672bb116e9b"
 };
 
 self.addEventListener('activate', function (event) {
